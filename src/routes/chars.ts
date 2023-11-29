fcb929f477f9d08bea6efa9f7e4bca090159fcda
joaocunha <joao.scunha@uvvnet.com.br>
2023-11-29 20:52:49 -0300

    Feat: modifying requests and removing the seasons table

--- src/routes/chars.ts
+++ src/routes/chars.ts
@@ -1,16 +1,13 @@
 import { FastifyInstance } from 'fastify'
 import { prisma } from '../lib/prisma'
-import { number, string, z } from 'zod'
+import { z } from 'zod'
 
 export async function CharsRoutes(app: FastifyInstance) {
   // Endpoint para obter todos os personagens
   app.get('/chars', async () => {
     const chars = await prisma.characters.findMany({
-      include: {
-        seasons: true,
-      },
       orderBy: {
-        name: 'desc',
+        name: 'asc',
       },
     })
 
@@ -18,11 +15,10 @@ export async function CharsRoutes(app: FastifyInstance) {
       return {
         id: char.id,
         name: char.name,
-        description: char.description.substring(0, 200).concat('...'),
+        description: char.description.substring(0, 350).concat('...'),
         imageURL: char.imageURL,
         type: char.type,
         actorName: char.actorName,
-        seasons: char.seasons.map((season) => season.season),
       }
     })
   })
@@ -39,9 +35,6 @@ export async function CharsRoutes(app: FastifyInstance) {
       where: {
         id,
       },
-      include: {
-        seasons: true,
-      },
     })
 
     return {
@@ -51,7 +44,6 @@ export async function CharsRoutes(app: FastifyInstance) {
       imageURL: char.imageURL,
       type: char.type,
       actorName: char.actorName,
-      seasons: char.seasons.map((season) => season.season),
     }
   })
 
@@ -63,11 +55,11 @@ export async function CharsRoutes(app: FastifyInstance) {
       imageURL: z.string(),
       type: z.string(),
       actorName: z.string(),
-      seasons: z.array(z.number()), // Modifique para aceitar números inteiros
     })
 
-    const { description, name, imageURL, type, actorName, seasons } =
-      bodySchema.parse(req.body)
+    const { description, name, imageURL, type, actorName } = bodySchema.parse(
+      req.body,
+    )
 
     const createdChar = await prisma.characters.create({
       data: {
@@ -76,12 +68,6 @@ export async function CharsRoutes(app: FastifyInstance) {
         description,
         imageURL,
         type,
-        seasons: {
-          create: seasons.map((season) => ({ season })),
-        },
-      },
-      include: {
-        seasons: true,
       },
     })
 
@@ -92,7 +78,6 @@ export async function CharsRoutes(app: FastifyInstance) {
       imageURL: createdChar.imageURL,
       type: createdChar.type,
       actorName: createdChar.actorName,
-      seasons: createdChar.seasons.map((season) => season.season),
     }
   })
 
@@ -110,11 +95,11 @@ export async function CharsRoutes(app: FastifyInstance) {
       imageURL: z.string(),
       type: z.string(),
       actorName: z.string(),
-      seasons: z.array(z.number()),
     })
 
-    const { description, name, imageURL, type, actorName, seasons } =
-      bodySchema.parse(req.body)
+    const { description, name, imageURL, type, actorName } = bodySchema.parse(
+      req.body,
+    )
 
     await prisma.characters.update({
       where: {
@@ -126,46 +111,22 @@ export async function CharsRoutes(app: FastifyInstance) {
         description,
         imageURL,
         type,
-        seasons: {
-          deleteMany: {}, // Deleta todas as associações de temporadas existentes
-          create: seasons.map((season) => ({ season })),
-        },
-      },
-      include: {
-        seasons: true,
       },
     })
   })
 
   // Endpoint para deletar um personagem por ID
-  // app.delete('/chars/:id', async (req) => {
-  //   const paramsSchema = z.object({
-  //     id: z.string().uuid(),
-  //   })
-  //   const paramsSchemas = z.object({
-  //     id: z.string().uuid(),
-  //   })
+  app.delete('/chars/:id', async (req) => {
+    const paramsSchema = z.object({
+      id: z.string().uuid(),
+    })
 
-  //   const { id } = paramsSchema.parse(req.params)
+    const { id } = paramsSchema.parse(req.params)
 
-  //   // Desassocie o personagem das temporadas sem excluir as temporadas
-  //   await prisma.characters.update({
-  //     where: {
-  //       id,
-  //     },
-  //     data: {
-  //       seasons: {
-  //         disconnect: [],
-  //       },
-  //     },
-  //   })
-  //   console.log()
-
-  //   // Agora você pode excluir o próprio personagem
-  //   await prisma.characters.delete({
-  //     where: {
-  //       id,
-  //     },
-  //   })
-  // })
+    await prisma.characters.delete({
+      where: {
+        id,
+      },
+    })
+  })
 }
