ce7e5d188c59645280cd7e033a0e6bbd5824bf68
joaocunha <joao.scunha@uvvnet.com.br>
2023-12-21 20:23:16 -0300

    feat: deixando a api publica

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import fastify from 'fastify'
+import cors from '@fastify/cors'
 import { CharsRoutes } from './routes/chars'
 
 const app = fastify()
+
+app.register(cors, {
+  origin: true,
+})
 
 app.register(CharsRoutes)
 
